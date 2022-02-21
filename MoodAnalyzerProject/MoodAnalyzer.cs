456bb52ee3fc0c201717190c8a0fab1d0be72205
gorenm49 <gorenityanand96@gmail.com>
2022-02-21 13:30:16 +0530

    [Nityanand]: analyse and respond Happy or sad mood logic added

---
+++ MoodAnalyzerProject/MoodAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProject
+{
+    public class MoodAnalyzer
+    {
+        public string AnalyzeMood(string msg)
+        {
+            if (msg.Equals("Happy"))
+            {
+                return "Happy";
+            }
+            if(msg.Equals("Sad"))
+            {
+                return "Sad";
+            }
+            return null;
+        }
+    }
+}
